aa478a972d9ee972f4cbf257f29cbcc88d278b5f
Jeanine Burke <jvburke@wustl.edu>
2016-07-12 01:04:19 -0400

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -30,22 +30,94 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
-
+	
+		
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		Elements paragraphs = wf.fetchWikipedia(url);
+		List<String> visitedLinks = new ArrayList<String>();
 
 		Element firstPara = paragraphs.get(0);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
-
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		processParagraphs(paragraphs, url, visitedLinks);
+		
 	}
+	
+	public static void processParagraphs(Elements paragraphs, String url, List<String> visitedLinks)throws IOException{
+			
+		String nextLink = getNextLink(paragraphs, url, visitedLinks);
+		if(nextLink.equals("https://en.wikipedia.org/wiki/Philosophy")){
+				
+				printLinks(visitedLinks);	
+				System.out.println("You've reached philosophy!");
+				return;		
+		} 
+		if(nextLink == null){
+			printLinks(visitedLinks);
+			System.out.println("Exiting now");
+			return;
+		}
+		if(nextLink == ""){
+			printLinks(visitedLinks);
+			System.out.println("Could not find valid link. Exiting now");		
+			return;
+		}
+		
+		//System.out.println(nextLink);
+		
+
+		else{
+		
+			Elements nextParagraphs = wf.fetchWikipedia(nextLink);
+			processParagraphs(nextParagraphs, nextLink, visitedLinks);
+		}
+	
+	}
+	
+	public static String getNextLink(Elements paragraphs, String url, List<String> visitedLinks){
+	
+	
+		String nextLink = "";
+		Elements links = paragraphs.select("a");
+		for(int i = 0; i < links.size(); i++){
+			
+			boolean isItalics = links.get(i).hasAttr("i");
+			
+			if(!isItalics){
+				String pulledLink = links.get(i).attr("href"); 
+				if((pulledLink.contains("/wiki/")) &&  !(pulledLink.contains("Greek")) && !(pulledLink.contains("Latin"))){
+				
+					nextLink = "https://en.wikipedia.org" + links.get(i).attr("href");
+				
+					if(visitedLinks.contains(nextLink)){
+					
+						System.out.println("Oops! First link is a page we have already seen");
+						return null;	
+					}
+					else{
+						visitedLinks.add(nextLink);
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		return nextLink;
+	
+	}	
+	
+	public static void printLinks(List<String> visitedLinks){
+	
+		for(String link: visitedLinks){
+			
+			System.out.println(link);
+		}
+	
+	}
+	
+	
+	
 }
+
+
+
